916c68d450bda9418cd340ae58959f795775c510
zelal-erat <zelaleratt@gmail.com>
2025-02-19 16:16:02 +0300

    store.js ve reducerlar oluşturuldu

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
+import {thunk} from "redux-thunk";
+import logger from "redux-logger";
+import clientReducer from "./reducers/clientReducer";
+
+import shoppingCartReducer from "./reducers/shoppingCartReducer";
+import productReducer from "./reducers/productReducer ";
+
+// Tüm reducer'ları birleştiriyoruz
+const rootReducer = combineReducers({
+  client: clientReducer,
+  product: productReducer,
+  shoppingCart: shoppingCartReducer,
+});
+
+// Middleware ekleyerek store'u oluşturuyoruz
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+export default store;
